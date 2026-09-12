9f331b97131f61f7df23ad3d8138062aaec6f3e6
Avtera <69560119+Avtera@users.noreply.github.com>
2026-09-12 10:26:42 +0700

    Update 2. build.bat
    module changed to winotify for compatibility accross applications

--- 2. build.bat
+++ 2. build.bat
@@ -1,4 +1,4 @@
-pyinstaller --onefile --noconsole --distpath . --collect-all win10toast --name CopyLinktoClipboard copylinktoclipboard.py
+pyinstaller --onefile --noconsole --distpath . --collect-all winotify --name CopyLinktoClipboard copylinktoclipboard.py
 rmdir /s /q build
 del /q CopyLinktoClipboard.spec
 
