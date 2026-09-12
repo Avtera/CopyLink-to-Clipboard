13d5dd43e2c4b1a2374b74c60d9e99d7adfc6122
Avtera <69560119+Avtera@users.noreply.github.com>
2026-09-12 10:27:36 +0700

    Update copylinktoclipboard.py
    minor spelling

--- copylinktoclipboard.py
+++ copylinktoclipboard.py
@@ -4,5 +4,5 @@ import pyperclip, sys
 url = sys.argv[1] if len(sys.argv) > 1 else sys.exit(0)
 pyperclip.copy(url)
 
-toast = Notification(app_id="Link Copier", title="Link Copied!", msg=f"URL: {url}")
+toast = Notification(app_id="Copy Link to Clipboard", title="Link Copied!", msg=f"URL: {url}")
 toast.show()
