4bee7dbf53862b534c250475c270818d3c3e350a
Avtera <69560119+Avtera@users.noreply.github.com>
2026-09-12 11:02:01 +0700

    Update copylinktoclipboard.py
    it is definetly an url, why do i need to state it is the url. dumb

--- copylinktoclipboard.py
+++ copylinktoclipboard.py
@@ -4,6 +4,6 @@ import pyperclip, sys
 url = sys.argv[1] if len(sys.argv) > 1 else sys.exit(0)
 pyperclip.copy(url)
 
-toast = Notification(app_id="Copy Link to Clipboard", title="Link Copied!", msg=f"URL: {url}")
+toast = Notification(app_id="Copy Link to Clipboard", title="Link Copied!", msg=f"{url}")
 toast.set_audio(audio.Default, loop=False)
 toast.show()
