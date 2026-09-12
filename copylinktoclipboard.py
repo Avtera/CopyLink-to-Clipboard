97bf6d4978e6532d51c2baf453b71306cafbeead
Avtera <69560119+Avtera@users.noreply.github.com>
2026-09-12 10:43:48 +0700

    Update copylinktoclipboard.py
    forget to add notification sound

--- copylinktoclipboard.py
+++ copylinktoclipboard.py
@@ -1,8 +1,9 @@
-from winotify import Notification
+from winotify import Notification, audio
 import pyperclip, sys
 
 url = sys.argv[1] if len(sys.argv) > 1 else sys.exit(0)
 pyperclip.copy(url)
 
 toast = Notification(app_id="Copy Link to Clipboard", title="Link Copied!", msg=f"URL: {url}")
+toast.set_audio(audio.Default, loop=False)
 toast.show()
